525e705e8152b0bebba2c7e7e24a628b437f9b34
dawidpacia <dawid.pacia@brainly.com>
2020-03-06 17:00:32 +0100

    calculator tests

--- day_1/src/calculator_class.py
+++ day_1/src/calculator_class.py
@@ -1,15 +1,30 @@
 class Calculator:
 
+
+    def check_type(self, a, b):
+        if type(a) not in [int, float] or type(b) not in [int, float]:
+            return False
+
     def add(self, a, b):
+        if not self.check_type(a, b):
+            return False
         return a + b
 
     def sub(self, a, b):
+        if not self.check_type(a, b):
+            return False
         return a - b
 
     def mul(self, a, b):
+        if not self.check_type(a, b):
+            return False
         return a * b
 
     def div(self, a, b):
+        if not self.check_type(a, b):
+            return False
+        if b == 0:
+            return False
         return a / b
 
 
--- day_1/tests/calculator_test.py
+++ day_1/tests/calculator_test.py
@@ -2,11 +2,26 @@ import unittest
 from hamcrest import *
 from day_1.src.calculator_class import Calculator
 
+
 class CalculatorTests(unittest.TestCase):
 
-    def test_sample(self):
-        assert_that(1, equal_to(1))
+    def setUp(self):
+        self.calc = Calculator()
 
+    def test_zero_division(self):
+        assert_that(self.calc.div(1, 0), equal_to(False))
+
+    def test_string(self):
+        assert_that(self.calc.add(1, "1"), equal_to(False))
+
+    def test_float(self):
+        assert_that(self.calc.add(1, 1.0), equal_to(2.0))
+
+    def test_mul(self):
+        assert_that(self.calc.mul(-1, -1), equal_to(1))
+
+    def tearDown(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
