27884e8fe96caa2ae12fff8cf02e5fc56fe0dcc5
paciadawid <paciadawid@gmail.com>
2020-03-06 12:34:44 +0100

    1st

---
+++ basic_data_types.py
@@ -0,0 +1,16 @@
+a_int = 1  # int (unlimited value)
+a_float = 1.0  # float (no double)
+a_string = "1"  # string (no char)
+a_bool = True  # boolean
+a_list = [1, "2", 3, 4.0]  # list
+print(a_list[1])  # 2nd element
+print(a_list[-1])  # last element
+a_dict = {  # dictionary
+    "value_1": 1,
+    "value_2": 2
+}
+print(a_dict["value_1"])  # value for key 'value_1'
+a_tuple = (1, 2)  # tuple
+print(a_tuple[0])
+
+print("i like  testing\"")
---
+++ for_loop.py
@@ -0,0 +1,11 @@
+a_list = [1, 2, 3, 4]
+a_string = "testowanie"
+
+# for value in a_list:
+#     print(value)
+
+# for letter in a_string:
+#     print(letter)
+
+for i in range(1, 11):
+    print(i)
